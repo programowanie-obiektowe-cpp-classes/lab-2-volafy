bd5ebd00bf61299c911f5b6b5d7f9b27781c296b
volafy <92111292+volafy@users.noreply.github.com>
2023-11-17 19:29:07 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,44 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    private:
+
+        Resource* resource;
+
+    public:
+
+        ResourceManager() : resource(new Resource()){}; //konstruktor
+
+        ResourceManager(const ResourceManager& resourceManager): resource(new Resource(*resourceManager.resource)){}; //konstruktor domyslny
+
+        ResourceManager& operator=(const ResourceManager& resourceManager){
+
+            if (this != &resourceManager) {
+                delete resource;//brak wyciekow pamieci :))
+                resource = new Resource(*resourceManager.resource);
+            }
+
+            return *this;
+        };
+
+        ResourceManager(ResourceManager&& resourceManager) : resource(resourceManager.resource){
+            resourceManager.resource = nullptr;
+        }
+
+        ResourceManager& operator=(ResourceManager&& resourceManager){
+            if (this != &resourceManager) {
+
+                delete resource;//brak wyciekow pamieci :))
+                resource = resourceManager.resource;
+                resourceManager.resource = nullptr;
+            }
+            return *this;
+        }
+
+        ~ResourceManager(){delete resource;}; //brak wyciekow pamieci :))
+
+        double get() const { return resource->get(); }
+
+
 };
+
